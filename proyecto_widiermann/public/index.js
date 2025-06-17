ca52ca0fcec661f27a61182ce5dacf526b387a7e
luly29 <raimondilourdes@gmail.com>
2025-06-17 20:06:18 -0300

    Agrego index y app

---
+++ proyecto_widiermann/public/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../proyecto_widiermann/App';
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle,
+*{ 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+  body {
+  font-family: 'Open Sans', sans-serif;
+} ;
