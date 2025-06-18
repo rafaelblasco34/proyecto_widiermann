a8988376df6873b0d09bee2e1e8be6f70bc8c1d3
programercat2006 <celiztiagonqn@gmail.com>
2025-06-18 07:24:49 -0300

    terminando codigo app

--- proyecto_widiermann/App.jsx
+++ proyecto_widiermann/App.jsx
@@ -1,7 +1,22 @@
-
+import React from 'react';
+import styled from 'styled-components';
+import LoginForm from './components/LoginForm';
 function App(){
-    return(
-        <h1>Hola</h1>
-    );
+    const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #1e3c72 0%, #2a5298 100%);
+  font-family: 'Open Sans', sans-serif;
+`;
+
+const App = () => {
+  return (
+    <AppContainer>
+      <LoginForm />
+    </AppContainer>
+  );
+};
 }
 export default App;
