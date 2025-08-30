6db2c7245fb57b271ca8db9d92d41473f3b73b7d
Giu277 <giulianapradoepet@gmail.com>
2025-08-30 20:39:43 -0300

    declaro Login y state f

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext.jsx";
+export default function Login() {
+  const [f, setF] = useState({ username: "", password: "" });
