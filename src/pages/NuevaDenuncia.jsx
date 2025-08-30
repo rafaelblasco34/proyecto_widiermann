75c6e35de6d4d26ed1095c96b541354d72c1d92e
Giu277 <giulianapradoepet@gmail.com>
2025-08-30 20:29:53 -0300

    armo handleChange y arranco handleSubmit

--- src/pages/NuevaDenuncia.jsx
+++ src/pages/NuevaDenuncia.jsx
@@ -3,3 +3,6 @@ import { useState } from "react";
 export default function NuevaDenuncia() {
       const [form, setForm] = useState({ titulo: "", descripcion: "", comisaria: "" });
 }
+ const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
+  const handleSubmit = e => { 
+    
