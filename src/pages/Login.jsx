21117089465e0b1c4598fab08e9e55ddccd2006d
Giu277 <giulianapradoepet@gmail.com>
2025-08-30 20:45:29 -0300

    boton entrar, texto usuarios y cierro

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,3 +21,8 @@ export default function Login() {
       <><input name="username" placeholder="Usuario" className="w-full border p-2 rounded-xl"
     value={f.username} onChange={onChange} /><input type="password" name="password" placeholder="Contraseña" className="w-full border p-2 rounded-xl"
     value={f.password} onChange={onChange} /></>
+    <button className="btn btn-primary w-full">Entrar</button>
+      <p className="text-sm opacity-70">Usuarios de prueba: <b>admin/1234</b>, <b>usuario/abcd</b></p>
+    </form>
+  );
+}
