c6b22854eb21760cb634666312e237eb5cf52f67
Giu277 <giulianapradoepet@gmail.com>
2025-08-30 20:44:03 -0300

    input usuario con value y change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,3 +18,5 @@ export default function Login() {
     <form onSubmit={onSubmit} className="card space-y-4 max-w-md mx-auto"></form> )
     <h2 className="text-xl font-semibold">Ingresar</h2>;
       {err && <p className="text-red-600">{err}</p>}
+      <input name="username" placeholder="Usuario" className="w-full border p-2 rounded-xl"
+             value={f.username} onChange={onChange} />
