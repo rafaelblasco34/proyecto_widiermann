9ffec42720a3e6ea1dd5cb0fd24d201ed7c54c9a
programercat2006 <celiztiagonqn@gmail.com>
2025-09-03 09:32:08 -0300

    cerrando el return con un section

--- src/pages/Denuncias.jsx
+++ src/pages/Denuncias.jsx
@@ -77,8 +77,12 @@ export default function Denuncias() {
 import { Link } from "react-router-dom";
 export default function Denuncias() {
   return (
-    <h2 className="text-xl font-semibold mb-2">Denuncias</h2>
+    <section className="card"></section>
+
+      <h2 className="text-xl font-semibold mb-2">Denuncias</h2>
       <p className="mb-4">Creá una nueva denuncia o consultá el procedimiento.</p>
       <Link to="/denuncias/nueva" className="btn btn-primary">Nueva denuncia</Link>
+
+     </section>
   );
 }
