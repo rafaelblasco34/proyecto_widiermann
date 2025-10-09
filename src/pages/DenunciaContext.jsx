46bb17c7361d1548dc094d07c57e52f720703855
cirojoaquin.tardugno@gmail.com <cirojoaquin.tardugno@gmail.com>
2025-10-09 20:14:50 -0300

    agregue contenido sobre el contexto

--- src/pages/DenunciaContext.jsx
+++ src/pages/DenunciaContext.jsx
@@ -1,7 +1,36 @@
-// denunciaContext.js
-import { createContext, useContext } from "react";
+// DenunciaContext.js
+import { createContext, useContext, useState } from "react";
 
 const DenunciaContext = createContext();
+
+export function DenunciaProvider({ children }) {
+  const [denunciaEnCurso, setDenunciaEnCurso] = useState(null);
+  const [listaDenuncias, setListaDenuncias] = useState([]);
+  const [tipos, setTipos] = useState([
+    "robo",
+    "secuestro",
+    "alteracion",
+    "violencia",
+    "siniestro",
+    "otros"
+  ]);
+  const [comisarias, setComisarias] = useState([
+    "Comisaría central",
+    "Comisaría neuquen",
+    "Comisaría balsa las perlas",
+    "Comisaría plottier"
+  ]);
+
+  return (
+    <DenunciaContext.Provider value={{
+      denunciaEnCurso, setDenunciaEnCurso,
+      listaDenuncias, setListaDenuncias,
+      tipos, comisarias
+    }}>
+      {children}
+    </DenunciaContext.Provider>
+  );
+}
 
 export function useDenuncia() {
   return useContext(DenunciaContext);
