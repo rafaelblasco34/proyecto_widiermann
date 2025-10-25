08c8eb0842f786e47a837ab3bd86d14cb8eb8210
rafaelblasco34 <rafaelblascohugo@gmail.com>
2025-10-25 12:33:07 -0300

    Creacion de test-tailwin para prueba de tailwind y box de introduccion a la pagina

---
+++ src/test-tailwind.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './test-styles.css';
+
+const TestTailwind = () => {
+  return (
+    <div className="test-tailwind">
+      <h1>Bienvenidos a DenunciasOnline</h1>
+        <p>¡Realizá denuncias y contactá comisarías sin moverte de tu casa!</p>
+      </div>
+  );
+};
+
+export default TestTailwind;
