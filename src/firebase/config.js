fbd92b203ba538592759b842ae95b314413a6637
cirojoaquin.tardugno@gmail.com <cirojoaquin.tardugno@gmail.com>
2025-10-26 18:57:07 -0300

    exportar servicios de firebase (firestore, Auth, Storage)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -13,3 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app)
+src/firebase/firestoreService.js
+
