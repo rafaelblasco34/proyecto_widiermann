4dec877504a78d66e2d91168a0953cbb0ef68817
rafaelblasco34 <rafaelblascohugo@gmail.com>
2025-10-27 16:36:30 -0300

    Modifico y corrijo index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App.jsx';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+// Inicializar Firebase
+import './firebase/config.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
