6f3fbb44dfaafec33a55726f7385afa2158659fa
rafaelblasco34 <rafaelblascohugo@gmail.com>
2025-11-04 23:22:29 -0300

    firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAkYKDv-ewuBxiQWojq1NHi64BJRcuUyf8",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "proyecto-widi-b83ae.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "proyecto-widi-b83ae",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "proyecto-widi-b83ae.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "631199394536",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:631199394536:web:894399e5dc16c813f9f75d"
-};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
